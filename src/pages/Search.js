c94e33adec83ac7653214c9ca0fcf03d4c4935a2
Sergio_Rodrigues <rodrigues.sergiofilho@gmail.com>
2021-09-20 01:51:47 -0300

    Implementando a barra de pesquisa (Req5)

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,10 +2,45 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 class Search extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      search: '',
+    };
+  }
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  };
+
   render() {
+    const { search } = this.state;
+    const minLetters = 2;
     return (
       <div data-testid="page-search">
         <Header />
+        <form>
+          <input
+            type="text"
+            data-testid="search-artist-input"
+            name="search"
+            value={ search }
+            onChange={ this.handleChange }
+          />
+          <button
+            type="button"
+            value="procurar"
+            data-testid="search-artist-button"
+            disabled={ search.length < minLetters }
+            placeholder="Nome do Artista ou Banda"
+          >
+            Procurar
+          </button>
+        </form>
       </div>
     );
   }
